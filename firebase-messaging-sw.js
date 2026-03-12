5db34b953d0e29a7eec1a73ba48cce119999481f
ashmi2005412 <ddhas800@gmail.com>
2026-03-12 22:50:34 +0530

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCQs1zTv68Xz_Jci28mIqHkp9tvcGJRTuM",
+  authDomain: "coffee-tower.firebaseapp.com",
+  projectId: "coffee-tower",
+  storageBucket: "coffee-tower.firebasestorage.app",
+  messagingSenderId: "300273625584",
+  appId: "1:300273625584:web:9ba3d6a293b00f3cf89535"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
